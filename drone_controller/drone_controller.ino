4dd4c7f68b55b4f688b811e8e53428d82146479b
Levente Tempfli <2000levi2000@gmail.com>
2019-01-26 21:41:31 +0200

    Program project created
    Program project created in Visual Studio 2017.
    Application type: Arduino Project
    Programming Language: C++
    Created: drone_controller.ino with the setup() and loop() functions

---
+++ drone_controller/drone_controller.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		drone_controller.ino
+ Created:	1/26/2019 9:35:18 PM
+ Author:	LEEVII
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
