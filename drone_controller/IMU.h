d3b3fbf6ac90242656a071a15c21593de297cd9b
Levente Tempfli <2000levi2000@gmail.com>
2019-07-19 15:24:27 +0300

    Added compass functionality
    The magnetometer is now used by the IMU class.
    Modified files: IMU.h, IMU.cpp, config.h.

--- drone_controller/IMU.h
+++ drone_controller/IMU.h
@@ -3,9 +3,11 @@
 
 #include <stdint.h>
 #include "I2C1_manager.h"
+#include "I2C_manager.h"
 #include "timer_sub.h"
 
 extern i2c1_manager _i2c1_mng;
+extern i2c_manager _i2c_mng;
 
 class imu {
 public:
@@ -24,11 +26,20 @@ private:
 	void read_raw_datas();
 	timer_sub timer_s = timer_sub(IMU_TIMER_BEGINTIME, IMU_TIMER_ENDTIME, IMU_TIMER_BEGINBLOCK, IMU_TIMER_ENDBLOCK, IMU_TIMER_WARNINGTIME);
 	int16_t gyro_raw_x, gyro_raw_y, gyro_raw_z, acc_raw_x, acc_raw_y, acc_raw_z, temp_raw;
+	double new_acc_x, new_acc_y;
 	double gyro_offset_x, gyro_offset_y, gyro_offset_z;
 	double gyro_transfer_x, gyro_transfer_y, gyro_transfer_z;
 	double dt;
 	double angle_x, angle_y, angle_z;
+	double angle_x_dam, angle_y_dam;
 	double acc_x, acc_y, temp;
+public:
+	int16_t mag_raw_x, mag_raw_y, mag_raw_z;
+	double mag_corr_x, mag_corr_y, mag_corr_z, mag_real_x, mag_real_y;
+	double compass_heading;
+	void calc_magn();
+	void yaw_fusion();
+	double course_deviation(double course_c, double course_b);
 };
 
 #endif // !IMU_H
--- drone_controller/config.h
+++ drone_controller/config.h
@@ -6,10 +6,10 @@
 //ESC Controller - escct
 constexpr uint16_t ESCCT_MINTHROTTLE = 100;
 constexpr uint16_t ESCCT_MAXTHROTTLE = 900;
-constexpr uint8_t ESCCT_ESC1PIN = 2;
-constexpr uint8_t ESCCT_ESC2PIN = 3;
-constexpr uint8_t ESCCT_ESC3PIN = 4;
-constexpr uint8_t ESCCT_ESC4PIN = 5;
+constexpr uint8_t ESCCT_ESC1PIN = 5;
+constexpr uint8_t ESCCT_ESC2PIN = 2;
+constexpr uint8_t ESCCT_ESC3PIN = 3;
+constexpr uint8_t ESCCT_ESC4PIN = 4;
 constexpr uint32_t ESCCT_INIT_WAIT_TIME = 5000000;
 
 constexpr uint64_t ESCCT_TIMER_BEGINTIME = 4120;
@@ -20,7 +20,7 @@ constexpr uint64_t ESCCT_TIMER_WARNINGTIME = 4300;
 
 //MAIN TIMER - timer_main
 constexpr bool TIMER_MAIN_ENDBLOCK = 1;
-constexpr uint64_t TIMER_MAIN_TURNTOTAL = 5000;
+constexpr uint64_t TIMER_MAIN_TURNTOTAL = 4000;
 constexpr uint8_t TIMER_SUB_TS_ARRAYLENGTH = 5;
 
 //I2C manager - i2c_manager
@@ -34,21 +34,43 @@ constexpr uint8_t I2C1_MANAGER_MAXDATALENGTH = 15;
 constexpr uint32_t I2C1_MANAGER_CLOCK = 400000;
 
 //IMU - imu
-constexpr uint8_t IMU_I2C_ADDRESS = 0x68;
-constexpr uint8_t IMU_WHO_AM_I = 0x75;
-constexpr uint8_t IMU_CONFIG_N = 5;
-constexpr uint8_t IMU_CONFIG_REGADR[IMU_CONFIG_N] = { 0x19, 0x1A, 0x1B, 0x1C, 0x6B };
-constexpr uint8_t IMU_CONFIG_REGVAL[IMU_CONFIG_N] = { 0x07, 0x00, 0x00 ,0x00 ,0x01 };
-constexpr double IMU_DEFGOFFSET_X= -127.28;
-constexpr double IMU_DEFGOFFSET_Y= -112.68;
-constexpr double IMU_DEFGOFFSET_Z= -19.95;
-constexpr bool IMU_INIT_OFFSETCALC = 1;
-constexpr uint16_t IMU_INIT_OFFSET_SAMPLE_N = 2000;
-constexpr uint8_t IMU_GYRO_SENSITIVITY_SCALE = 131;
-constexpr double IMU_COMPL_FILTER_ALFA = 0.93;
-constexpr double IMU_COMPL_FILTER_1MALFA = 0.07;
-constexpr uint64_t IMU_TIMER_BEGINTIME = 3000;
-constexpr uint64_t IMU_TIMER_ENDTIME = 4000;
+constexpr uint8_t IMU_ACGY_I2C_ADDRESS = 0x68;
+constexpr uint8_t IMU_MAGN_I2C_ADDRESS = 0x1E;
+constexpr uint8_t IMU_ACGY_WHO_AM_I = 0x75;
+constexpr uint8_t IMU_MAGN_IDEN_STADR = 0X0A;//3 registers are read
+constexpr int32_t IMU_MAGN_IDEN_VAL = 4731955;
+constexpr uint64_t IMU_INIT_DELAY_SHORT = 100000;
+constexpr uint8_t IMU_ACGY_CONFIG_N = 4;
+constexpr uint8_t IMU_ACGY_CONFIG_REGADR[IMU_ACGY_CONFIG_N] = { 0x6B, 0x1B, 0x1C, 0x1A };
+constexpr uint8_t IMU_ACGY_CONFIG_REGVAL[IMU_ACGY_CONFIG_N] = { 0x00, 0x08 ,0x10, 0x03 };
+constexpr uint8_t IMU_MAGN_CONFIG_N = 3;
+constexpr uint8_t IMU_MAGN_CONFIG_REGADR[IMU_ACGY_CONFIG_N] = { 0x00, 0x01, 0x02 };
+constexpr uint8_t IMU_MAGN_CONFIG_REGVAL[IMU_ACGY_CONFIG_N] = { 0x78, 0x20 ,0x00 };//Regiser A bits as 01111000 to set sample rate (average of 8 at 75Hz). Regiser B bits as 00100000 to set the gain at +/-1.3Ga.  Mode Regiser bits as 00000000 to set Continues-Measurement Mode. 
+constexpr uint8_t IMU_ACGY_READRAW_REGVAL = 0x3B;//14 values are read
+constexpr double IMU_GYR_OFFSET_X = -91.92;
+constexpr double IMU_GYR_OFFSET_Y = -62.97;
+constexpr double IMU_GYR_OFFSET_Z = -48.52;
+constexpr int16_t IMU_ACC_OFFSET_X = 279;
+constexpr int16_t IMU_ACC_OFFSET_Y = -70;
+constexpr bool IMU_GYRO_INIT_OFFSETCALC = 1;
+constexpr uint16_t IMU_GYRO_INIT_OFFSET_SAMPLE_N = 2000;
+constexpr uint64_t IMU_GYRO_INIT_OFFSET_DELAY = 500;
+constexpr uint8_t IMU_GYRO_SENSITIVITY_SCALE = 65.5;
+constexpr double IMU_COMPL_FILTER_ALFA = 0.98;
+constexpr double IMU_COMPL_FILTER_1MALFA = 0.02;
+constexpr double IMU_DAMPEN_ANG_ALFA = 0.9;
+constexpr double IMU_DAMPEN_ANG_1MALFA = 0.1;
+constexpr uint8_t IMU_MAGN_READRAW_REGVAL = 0x03;//6 values are read
+constexpr double IMU_MAGN_OFFSET_X = -126;
+constexpr double IMU_MAGN_OFFSET_Y = -0.5;
+constexpr double IMU_MAGN_OFFSET_Z = 128;
+constexpr double IMU_MAGN_SCALE_X = 1.068181818;
+constexpr double IMU_MAGN_SCALE_Y = 0.887947883;
+constexpr double IMU_MAGN_SCALE_Z = 1.066510172;
+constexpr double IMU_YAW_FUSION_ALFA = 0.98;
+constexpr double IMU_YAW_FUSION_1MALFA = 0.02;
+constexpr uint64_t IMU_TIMER_BEGINTIME = 2500;
+constexpr uint64_t IMU_TIMER_ENDTIME = 3850;
 constexpr bool IMU_TIMER_BEGINBLOCK = 1;
 constexpr bool IMU_TIMER_ENDBLOCK = 1;
 constexpr uint64_t IMU_TIMER_WARNINGTIME = 3700;
@@ -103,7 +125,8 @@ constexpr uint64_t GPS_TIMER_WARNINGTIME = 2275;
 
 
 //Debug messages
-constexpr char DBG_IMU_INIT_ERROR[] = "IMU - INIT - Error at communication with the sensor";
+constexpr char DBG_IMU_ACGY_INIT_ERROR[] = "IMU - INIT - Error at communication with the accelerometer & gyroscope";
+constexpr char DBG_IMU_MAGN_INIT_ERROR[] = "IMU - INIT - Error at communication with the magnetometer";
 constexpr char DBG_IMU_OFFSCAL_RESULT[] = "IMU - INIT - New gyro calculated offsets(XYZ):";
 
 #endif // !CONFIG_H
